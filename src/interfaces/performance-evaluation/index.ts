a61b91427f380576ec1a6ad86840c749afe7adc5
roq-ai <>
2023-08-03 13:24:56 +0000

    Initial commit

---
+++ src/interfaces/performance-evaluation/index.ts
@@ -0,0 +1,20 @@
+import { UserInterface } from 'interfaces/user';
+import { GetQueryInterface } from 'interfaces';
+
+export interface PerformanceEvaluationInterface {
+  id?: string;
+  evaluation_date: any;
+  evaluation: string;
+  user_id?: string;
+  created_at?: any;
+  updated_at?: any;
+
+  user?: UserInterface;
+  _count?: {};
+}
+
+export interface PerformanceEvaluationGetQueryInterface extends GetQueryInterface {
+  id?: string;
+  evaluation?: string;
+  user_id?: string;
+}
